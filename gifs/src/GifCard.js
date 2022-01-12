06d6123e503e2ef4345ec0e67d207d113806022a
Zixin Zou <zxinmm@gmail.com>
2022-01-12 16:30:21 -0500

    gifs sample

--- gifs/src/GifCard.js
+++ gifs/src/GifCard.js
@@ -2,11 +2,13 @@ import React from "react"
 
 function GifCard (props) {
 
-    console.log(props.gifs[0].url)
+    console.log(props.gifs[0].images.original.url)
+
+    const display = props.gifs.map(gif=> <img src={gif.images.original.url}></img>)
 
     return (
         <div>
-            
+            <img src={props.gifs[0].images.original.url}></img>
         </div>
     )
 
