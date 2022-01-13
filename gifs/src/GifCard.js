3b2b90e3d59cff3155eae4d18909616422af1977
Zixin Zou <zxinmm@gmail.com>
2022-01-13 13:44:43 -0500

    new

--- gifs/src/GifCard.js
+++ gifs/src/GifCard.js
@@ -2,13 +2,14 @@ import React from "react"
 
 function GifCard (props) {
 
-    console.log(props.gifs[0].images.original.url)
+    console.log(props.gifs[0].images)
 
-    const display = props.gifs.map(gif=> <img src={gif.images.original.url}></img>)
+    const display = props.gifs.map((gif,index)=> <img key={index} src={gif.images.original.url}></img>)
 
     return (
         <div>
-            <img src={props.gifs[0].images.original.url}></img>
+            {/* <img src={props.gifs[0].images.original.url}></img> */}
+            {display}
         </div>
     )
 
